b179065c48d231a4adcdbad1401a2891d10a75a8
unknown <xxjuanmanderxx@gmail.com>
2024-04-01 13:29:41 -0500

    Numbers 1-10 in cycle

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
