dbd751b287ace3e20647543780ec510e6fb5963c
unknown <xxjuanmanderxx@gmail.com>
2024-04-01 13:34:25 -0500

    Randon number

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Random random = new Random();
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+        int number = random.nextInt(100);
+        System.out.println("Number: " + number);
     }
 }
